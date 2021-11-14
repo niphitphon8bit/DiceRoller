2bfbf84db1c5bfef5fecf38a292ed9e7724e9ce3
niphitphon8bit <niphitphon_nt@hotmail.com>
2021-11-14 14:28:58 +0700

    add Reset function to reset dice roll number to zero

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,27 +12,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_btn)
         val countUpBtn: Button = findViewById(R.id.count_up_btn)
-        countUpBtn.setOnClickListener{countUp()}
-        rollButton.setOnClickListener{rollDice()}
-
+        val resetBtn: Button = findViewById(R.id.reset_btn)
+        countUpBtn.setOnClickListener { countUp() }
+        rollButton.setOnClickListener { rollDice() }
+        resetBtn.setOnClickListener { resetDice() }
     }
 
-    private fun rollDice(){
+    private fun rollDice() {
         val randomInt = (1..6).random()
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
-        Toast.makeText(this, "button clicked",
-        Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+            this, "button clicked",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
-    private fun countUp(){
+    private fun countUp() {
         val resultText: TextView = findViewById(R.id.result_text)
-        if(resultText.text.toString() == "Hello World!") {
+        if (resultText.text.toString() == "Hello World!") {
             resultText.text = "1"
-        }else if(resultText.text.toString() != "6"){
+        } else if (resultText.text.toString() != "6") {
             var number = Integer.parseInt(resultText.text.toString())
             number += 1
             resultText.text = number.toString()
         }
+    }
+
+    private fun resetDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "0"
     }
 }
